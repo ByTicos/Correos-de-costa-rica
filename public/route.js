66bed5c6041b45c7e35718b69b1a4941dccd2d08
heinerPerezL <35514268+heinerPerezL@users.noreply.github.com>
2018-03-12 20:49:45 -0600

    inicio
    inicio de instalacion

---
+++ public/route.js
@@ -0,0 +1,68 @@
+(() => {
+  'use strict';
+  angular
+    .module('appRoutes', ['ui.router', 'oc.lazyLoad', 'uiRouterTitle'])
+    .config(routing);
+  routing.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function routing($stateProvider, $urlRouterProvider, $oclazyLoad) {
+
+    $stateProvider
+      .state('landingPage', {
+        url: '/',
+        templateUrl: './components/landingPage/landingPage.view.html',
+        data:{
+          pageTitle: 'Correos de Costa Rica'
+        }
+      })
+/*
+      .state('admin', {
+        url: '/admin',
+        templateUrl: './components/admin/admin.view.html',
+        data:{
+          pageTitle: 'Registro admin'
+        },
+        params: {
+          objUsuarioTemp:''
+        },
+        resolve: {
+          load: ['$ocLazyLoad', ($ocLazyLoad) => {
+            return $ocLazyLoad.load('./components/admin/admin.controller.js')
+          }]
+        },
+        controller: 'controladorAdministrador',
+        controllerAs: 'vm'
+      })
+*/
+     
+
+    
+/*
+      .state('logIn', {
+        url: '/logIn',
+        templateUrl: './components/logIn/logIn.view.html',
+        data:{
+          pageTitle: 'Iniciar sesión'
+        },
+        resolve: {
+          load: ['$ocLazyLoad', ($ocLazyLoad) => {
+            return $ocLazyLoad.load('./components/logIn/logIn.controller.js')
+          }]
+        },
+        controller: 'controladorLogin',
+        controllerAs: 'vm'
+      })
+
+
+*/
+     
+      
+
+    
+
+      
+
+    $urlRouterProvider.otherwise('/');
+  };
+
+})();
