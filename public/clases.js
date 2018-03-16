8d1d01aaf5f21b2fb3c16a41f30c9b9a2f722824
Jeremy Piedra Cordoba <trip807@hotmail.com>
2018-03-16 11:48:00 -0700

    Cambio clases
    Parametro pPuesto para los encargados de Aduana

---
+++ public/clases.js
@@ -0,0 +1,20 @@
+class Usuario{
+    constructor(pCedula, pFoto, pPrimerNombre, pSegundoNombre, pPrimerApellido, pSegundoApellido, pEmail, pTelefono, pfechaNacimiento, pProvincia, pCanton, pDistrito, pDireccionExacta, pTipo, pPuesto){
+        this.cedula = pCedula;
+        this.foto = pFoto;
+        this.primerNombre = pPrimerNombre;
+        this.segundoNombre = pSegundoNombre;
+        this.primerApellido = pPrimerApellido;
+        this.segundoApellido = pSegundoApellido;
+        this.correo = pEmail;
+        this.telefono = pTelefono;
+        this.fechaNacimiento = pfechaNacimiento;
+        this.provincia = pProvincia;
+        this.canton = pCanton;
+        this.distrito = pDistrito;
+        this.direccionExacta = pDireccionExacta;
+        this.tipo = pTipo;
+        this.puesto = pPuesto;
+    } 
+      
+}
