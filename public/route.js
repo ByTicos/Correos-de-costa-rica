cdec0ec042acf6f5c44229764de81b67940609a0
Jeremy Piedra Cordoba <trip807@hotmail.com>
2018-03-22 20:03:06 -0700

    Cambios Landing
    Estilos, Landing page, eliminar carpeta Aduanas vieja, Carpeta Encargado Aduanas con listar, modificar, registrar.

--- public/route.js
+++ public/route.js
@@ -285,24 +285,7 @@
         controllerAs: 'vm'
       })
     
-
-      .state('encargadoAduanas', {
-        url: '/encargadoAduanas',
-        templateUrl: './components/encargadoAduanas/encargadoAduanas.view.html',
-        data:{
-          pageTitle: 'Dashboard Encargado de Aduanas'
-        },
-        params: {
-          objUsuarioTemp: ''
-        },
-        resolve: {
-          load: ['$ocLazyLoad', ($ocLazyLoad) => {
-            return $ocLazyLoad.load('./components/encargadoAduanas/encargadoAduanas.controller.js')
-          }]
-        },
-        controller: 'controladorEncargadoAduanas',
-        controllerAs: 'vm'
-      })     
+    
 
       .state('sucursales', {
         url: '/sucursales',
@@ -393,6 +376,62 @@
         controller: 'controladorModificarEncargadoSucursal',
         controllerAs: 'vm',
       })
+
+      .state('registrarEncargadoAduana', {
+        url: '/registrarEncargadoAduana',
+        templateUrl: './components/usuarios/encargadoAduana/registrarEncargadoAduana/registrarEncargadoAduana.view.html',
+        data:{
+          pageTitle: 'Registro Encargado Aduana'
+        },
+        params: {
+          objUsuarioTemp: ''
+        },
+        resolve: {
+          load: ['$ocLazyLoad', ($ocLazyLoad) => {
+            return $ocLazyLoad.load('./components/usuarios/encargadoAduana/registrarEncargadoAduana/registrarEncargadoAduana.controller.js')
+          }]
+        },
+        controller: 'controladorRegistrarEncargadoAduana',
+        controllerAs: 'vm'
+      })
+
+      .state('listarEncargadoAduana', {
+        url: '/listarEncargadoAduana',
+        templateUrl: './components/usuarios/encargadoAduana/listarEncargadoAduana/listarEncargadoAduana.view.html',
+        data:{
+          pageTitle: 'Listar Encargado de Aduana'
+        },
+        params: {
+          objUsuarioTemp: ''
+        },
+        resolve: {
+          load: ['$ocLazyLoad', ($ocLazyLoad) => {
+            return $ocLazyLoad.load('./components/usuarios/encargadoAduana/listarEncargadoAduana/listarEncargadoAduana.controller.js')
+          }]
+        }, 
+        controller: 'controladorListarEncargadoAduana',
+        controllerAs: 'vm',
+      })
+
+      .state('modificarEncargadoAduana', {
+        url: '/modificarEncargadoAduana',
+        templateUrl: './components/usuarios/encargadoAduana/modificarEncargadoAduana/modificarEncargadoAduana.view.html',
+        data:{
+          pageTitle: 'Editar Encargado de Aduana'
+        },
+        params: {
+          objUsuarioTemp: ''
+        },
+        resolve: {
+          load: ['$ocLazyLoad', ($ocLazyLoad) => {
+            return $ocLazyLoad.load('./components/usuarios/encargadoAduana/modificarEncargadoAduana/modificarEncargadoAduana.controller.js')
+          }]
+        }, 
+        controller: 'controladorModificarEncargadoAduana',
+        controllerAs: 'vm',
+      })
+
+
 
       
 
