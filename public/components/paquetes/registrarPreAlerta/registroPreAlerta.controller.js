89deb9f3b45b1e03dff6cfce53bb127ab6d45494
heinerPerezL <35514268+heinerPerezL@users.noreply.github.com>
2018-03-24 18:39:29 -0600

    impuesto
    se trabaja en el  calculo de impuesto

--- public/components/paquetes/registrarPreAlerta/registroPreAlerta.controller.js
+++ public/components/paquetes/registrarPreAlerta/registroPreAlerta.controller.js
@@ -26,8 +26,27 @@
       error => {
         console.log('Ocurrió un error ' + error.data);
       }
-    );
+    )
+    
+    
+    console.log('porcentaje', vm.porcentajePaquete);
 
+    function calcular() {
+      let peso = vm.nuevoPaquete.peso;
+      console.log('peso', vm.nuevoPaquete.peso);
+      let precioXkilo = 2;
+      let porcentaje = vm.porcentajePaquete;
+      let monto = 0;
+      let impuestoTotal= 0;
+
+      
+
+
+      // monto = peso
+
+      return peso;
+
+    };
 
     vm.registrarPaquete = (pnuevoPaquete) => {
       let session = JSON.parse(sessionStorage.getItem('sesion'));
