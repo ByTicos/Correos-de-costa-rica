885af498e4f3083b8f3832c0ac2975ad78827f1d
heinerPerezL <35514268+heinerPerezL@users.noreply.github.com>
2018-03-25 20:52:05 -0600

    clases
    clases se desaparecen

--- public/components/paquetes/registrarPreAlerta/registroPreAlerta.controller.js
+++ public/components/paquetes/registrarPreAlerta/registroPreAlerta.controller.js
@@ -11,6 +11,10 @@
     let vm = this;
     vm.nuevoPaquete = {};
     vm.calculo = 0;
+    vm.transporte = 0;
+    vm.total = 0;
+    vm.ocultarcalculo = false;
+    
 
 
     vm.listaPreAlerta = () => {
@@ -33,17 +37,32 @@
     
 vm.calcular = (pnuevoPaquete) => {
   let calculo = 0;
+  let transporte = 0;
+  let total = 0;
   let impuesto = Number (pnuevoPaquete.tipoArticulo._id);
   let peso = Number (pnuevoPaquete.peso);
   let precio = Number (pnuevoPaquete.precio);
-  
-  
-  calculo = peso * precio;
-  console.log (pnuevoPaquete.tipoArticulo._id);
+  let kilometro = Number (pnuevoPaquete.kilometro);
 
-  vm.calculo = calculo;
+
+  
+
+  calculo = (precio * impuesto/100)+ precio + (peso * 1);
+  transporte = kilometro * 1.5;
+  total = calculo + transporte;
+  
+  vm.transporte = transporte;
+  vm.calculo = calculo.toFixed(2);
+  vm.total = total.toFixed(2);
+  
+
 };
 
+
+vm.ocultarCalculo = ()=>{
+  vm.ocultarcalculo = true;
+  
+}
 
 
 
@@ -52,7 +71,7 @@ vm.calcular = (pnuevoPaquete) => {
       let usuario = session.nombre;
 
 
-      let objNuevoPaquete = new Paquete(usuario, pnuevoPaquete.tracking, pnuevoPaquete.distribuidor, pnuevoPaquete.precio, pnuevoPaquete.peso, pnuevoPaquete.tipoArticulo, pnuevoPaquete.descripcion);
+      let objNuevoPaquete = new Paquete(usuario, pnuevoPaquete.tracking, pnuevoPaquete.distribuidor, pnuevoPaquete.precio, pnuevoPaquete.peso ,pnuevoPaquete.kilometro, pnuevoPaquete.tipoArticulo, pnuevoPaquete.descripcion);
      
      
       
