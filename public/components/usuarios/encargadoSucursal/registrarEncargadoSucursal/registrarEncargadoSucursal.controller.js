8b217eaffbb61b05792e429a1d1c842bf5b817b7
aaron.acuna <3audiovisual@gmail.com>
2018-03-26 00:19:05 -0600

    cambios por contrasenna

--- public/components/usuarios/encargadoSucursal/registrarEncargadoSucursal/registrarEncargadoSucursal.controller.js
+++ public/components/usuarios/encargadoSucursal/registrarEncargadoSucursal/registrarEncargadoSucursal.controller.js
@@ -16,7 +16,7 @@
 
     vm.registrarUsuario = (pNuevoUsuario) => {
 
-      let objNuevoUsuario = new Usuario(pNuevoUsuario.cedula, pNuevoUsuario.foto, pNuevoUsuario.primerNombre, pNuevoUsuario.segundoNombre, pNuevoUsuario.primerApellido, pNuevoUsuario.segundoApellido, pNuevoUsuario.correo, pNuevoUsuario.telefono, pNuevoUsuario.fechaNacimiento, pNuevoUsuario.provincia, pNuevoUsuario.canton, pNuevoUsuario.distrito,pNuevoUsuario.direccionExacta, '2',pNuevoUsuario.sucursalAsignada, pNuevoUsuario.puesto);
+      let objNuevoUsuario = new Usuario(pNuevoUsuario.cedula, pNuevoUsuario.foto, pNuevoUsuario.primerNombre, pNuevoUsuario.segundoNombre, pNuevoUsuario.primerApellido, pNuevoUsuario.segundoApellido, pNuevoUsuario.correo, pNuevoUsuario.telefono, pNuevoUsuario.fechaNacimiento, pNuevoUsuario.provincia, pNuevoUsuario.canton, pNuevoUsuario.distrito, pNuevoUsuario.direccionExacta, pNuevoUsuario.contrasenna, '2',pNuevoUsuario.sucursalAsignada);
 
       let registro = servicioUsuarios.addUsuario(objNuevoUsuario);
 
