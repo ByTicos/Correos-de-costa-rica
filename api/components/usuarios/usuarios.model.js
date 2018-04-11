21170e91c6dd9244731647f939a57677f7071cef
Luisk <ldelvalleu@ucenfotec.ac.cr>
2018-04-10 21:01:43 -0600

    Backend registrar usuario
    Back end registrar usuario.
    Creación de factory.

---
+++ api/components/usuarios/usuarios.model.js
@@ -0,0 +1,33 @@
+//Requerimos mongoose
+const mongoose = require('mongoose');
+
+//Esquema de usuarios
+var UserSchema = new mongoose.Schema({
+  cedula : {type : String, required : true},
+  foto : {type : String, required : true},
+  primerNombre : {type : String, required : true},
+  segundoNombre : {type : String},
+  primerApellido : {type : String, required : true},
+  segundoApellido : {type : String},
+  correo : {type : String, required : true},
+  telefono : {type : String, required : true},
+  fechaNacimiento : {type : String, required : true},
+  provincia : {type : String, required : true},
+  canton : {type : String, required : true},
+  distrito : {type : String, required : true},
+  direccionExacta : {type : String, required : true},
+  tipo : {type : String},
+  listaPaquetes : {type : Array, required : true},
+  sucursalAsignada : {type : String, required : true},
+  puesto : {type : String},
+  vehiculo : {type : String},
+  listaLicencias : {type : Array, required : true},
+  estado : {type : String, required : true},
+  listaTarjetas : {type : Array, required : true},
+  listaPaquetesConvenios : {type : Array, required : true},
+  contrasenna : {type : String, required : true}
+});
+
+//nombre del modelo dentro del back end y el userSchema es el nombre dentro de mongoose
+module.exports = mongoose.model('User', UserSchema); 
+//User va en mayúscula y singular aunque en la bd todo se pone en minúscula y plural
---
+++ public/components/factories/dataStorage.factory.js
@@ -0,0 +1,125 @@
+(() => {
+  'use strict';
+  angular
+    .module('correos')
+    .factory('dataStorageFactory', dataStorageFactory);
+
+  dataStorageFactory.$inject = ['$q', '$log', '$http'];
+
+  function dataStorageFactory($q, $log, $http) {
+
+    const localAPI = {
+      getUsersData: _getUsersData,
+      setUserData: _setUserData,
+      setSession: _setSession,
+      closeSession: _closeSession,
+      getSession: _getSession
+    };
+    return localAPI;
+
+    /**
+     * Funcion que obtiene los datos de los usuarios del back end y los retorna
+     */
+    function _getUsersData() {
+      let listaUsuarios = [];
+
+      let peticion = $.ajax({
+        url: 'http://localhost:4000/api/get_all_users',
+        type: 'get',
+        contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType: 'json',
+        async: false,
+        data: {}
+      });
+
+      peticion.done((usuarios) => {
+        console.log('Datos que vienen desde la base de datos')
+        console.log(usuarios);
+        listaUsuarios = usuarios;
+      });
+      peticion.fail(() => {
+        listaUsuarios = [];
+        console.log('Ocurrió un error');
+      });
+
+      return listaUsuarios;
+    }
+
+    function _setUserData(data) {
+      let response;
+
+      let peticion = $.ajax({
+        url: 'http://localhost:4000/api/save_user',
+        type: 'post',
+        contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType: 'json',
+        async: false,
+        data: {
+          'cedula': data.cedula,
+          'foto': data.foto,
+          'primerNombre': data.primerNombre,
+          'segundoNombre': data.segundoNombre,
+          'primerApellido': data.primerApellido,
+          'segundoApellido': data.segundoApellido,
+          'correo': data.correo,
+          'telefono':data.telefono,
+          'fechaNacimiento': data.fechaNacimiento,
+          'provincia': data.provincia,
+          'canton': data.canton,
+          'distrito': data.distrito,
+          'direccionExacta': data.direccionExacta,
+          'tipo': data.tipo,
+          'listaPaquetes': data.listaPaquetes,
+          'sucursalAsignada': data.sucursalAsignada,
+          'puesto': data.puesto,
+          'vehiculo': data.vehiculo,
+          'listaLicencias': data.listaLicencias,
+          'estado': data.estado,
+          'listaTarjetas': data.listaTarjetas,
+          'listaPaquetesConvenios': data.listaPaquetesConvenios,
+          'contrasenna': data.contrasenna,
+        }
+      });
+
+      peticion.done((datos) => {
+        response = datos.msj;
+        console.log('Petición realizada con éxito');
+      });
+      peticion.fail((error) => {
+        response = error;
+        console.log('Ocurrió un error');
+      });
+
+      return response;
+    }
+
+    /**
+     * Función que almacena las credenciales dentro del session Storage
+     * @param {Credenciales} value 
+     */
+    function _setSession(value) {
+      let response = true;
+      sessionStorage.setItem('session', JSON.stringify(value));
+      return response;
+    }
+
+    /**
+     * Función que elimina los datos de la sesión activa
+     */
+    function _closeSession() {
+      let response = true;
+      sessionStorage.removeItem('session');
+      return response;
+    };
+
+    /**
+     * Función que retorna los datos almacenados dentro del sessionStorage
+     */
+    function _getSession() {
+      let sessionActive = JSON.parse(sessionStorage.getItem('session'));
+
+      return sessionActive;
+    }
+
+  }
+})();
