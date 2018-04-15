237521c61962d0e07f2cef91d1903f9b2d7297ff
Jeremy Piedra Cordoba <trip807@hotmail.com>
2018-04-15 09:18:01 -0700

    Registrar encargado Aduana
    Cambios codigo

--- public/components/usuarios/encargadoAduana/registrarEncargadoAduana/registrarEncargadoAduana.controller.js
+++ public/components/usuarios/encargadoAduana/registrarEncargadoAduana/registrarEncargadoAduana.controller.js
@@ -1,33 +1,95 @@
+
 (() => {
   'use strict'
   angular
     .module('correos')
     .controller('controladorRegistrarEncargadoAduana', controladorRegistrarEncargadoAduana);
     
-    controladorRegistrarEncargadoAduana.$inject = ['$state', '$stateParams', '$location', 'servicioUsuarios'];
+  controladorRegistrarEncargadoAduana.$inject = ['$http','$state', '$stateParams', '$location', 'servicioUsuarios', 'imageService','Upload', 'servicioSucursales'];
 
-  function controladorRegistrarEncargadoAduana($state, $stateParams, $location, servicioUsuarios) {
+  function controladorRegistrarEncargadoAduana($http ,$state, $stateParams, $location, servicioUsuarios, imageService, Upload, servicioSucursales) {
     let vm = this;
 
     vm.nuevoUsuario = {};
+    servicioSucursales.listarSucursalesJson();
+    vm.listaSucursales = servicioSucursales.getSucursal();
+    console.log('Prueba', servicioSucursales.getSucursal());
+    vm.cloudObj = imageService.getConfiguration();
 
-    vm.registrarUsuario = (pNuevoUsuario) => {
+    vm.provincias = $http({
+      method: 'GET',
+      url: './sources/data/provincias.json'
+    }).then((success) => {
+      vm.provincias = success.data;
+    }, (error) => {
+      console.log("Ocurrió un error " + error.data);
+    });
 
-      let objNuevoUsuario = new Usuario(pNuevoUsuario.cedula, pNuevoUsuario.foto, pNuevoUsuario.primerNombre, pNuevoUsuario.segundoNombre, pNuevoUsuario.primerApellido, pNuevoUsuario.segundoApellido, pNuevoUsuario.correo, pNuevoUsuario.telefono, pNuevoUsuario.fechaNacimiento, pNuevoUsuario.provincia, pNuevoUsuario.canton, pNuevoUsuario.distrito,pNuevoUsuario.direccionExacta, pNuevoUsuario.contrasenna, '4', pNuevoUsuario.puesto);
+    vm.rellenarCantones = (pidProvincia) => {
+      vm.cantones = $http({
+        method: 'GET',
+        url: './sources/data/cantones.json'
+      }).then((success) => {
+        let cantones = [];
+        for (let i = 0; i < success.data.length; i++) {
+          if (pidProvincia == success.data[i].idProvincia) {
+            cantones.push(success.data[i]);
+          }
+        }
+        vm.cantones = cantones;
+      }, (error) => {
+        console.log("Ocurrió un error " + error.data)
+      });
+    }
+
+    vm.rellenarDistrito = (pidCanton) => {
+      console.log(pidCanton);
+      vm.distritos = $http({
+        method: 'GET',
+        url: './sources/data/distritos.json'
+      }).then((success) => {
+        let distritos = [];
+        for (let i = 0; i < success.data.length; i++) {
+          if (pidCanton == success.data[i].idCanton) {
+            distritos.push(success.data[i]);
+          }
+        }
+        vm.distritos = distritos;
+      }, (error) => {
+        console.log("Ocurrió un error " + error.data)
+      });
+    }
+
+
+    vm.registrarUsuario = (pNuevoUsuario/*, imgUrl*/) => {
+
+      let objNuevoUsuario = new Usuario(pNuevoUsuario.cedula, 'imgUrl', pNuevoUsuario.primerNombre, pNuevoUsuario.segundoNombre, pNuevoUsuario.primerApellido, pNuevoUsuario.segundoApellido, pNuevoUsuario.correo, pNuevoUsuario.telefono, pNuevoUsuario.fechaNacimiento, pNuevoUsuario.provincia, pNuevoUsuario.canton, pNuevoUsuario.distrito, pNuevoUsuario.direccionExacta, pNuevoUsuario.contrasenna, '2', pNuevoUsuario.sucursalAsignada);
 
       let registro = servicioUsuarios.addUsuario(objNuevoUsuario);
 
-      if (registro == true) {
-        swal("Registro exitoso", "El usuario ha sido registrado correctamente", "success", {
-          button: "Aceptar",
-        });
-        /*$location.path('/logIn');*/
+      if (registro == 'Se registró el usuario correctamente') {
+        let sesion = JSON.parse(sessionStorage.getItem('sesion'));
+        if(sesion == null || sesion.tipo != '5'){
+          
+          swal("Registro exitoso", "El usuario ha sido registrado correctamente", "success", {
+            button: "Aceptar",
+          }); 
+          $location.path('/logIn');
+        }
+        else{
+          swal("Registro exitoso", "El usuario ha sido registrado correctamente", "success", {
+            button: "Aceptar",
+          });
+          
+        }
+        
       }
       else {
         swal("Registro fallido", "Ha ocurrido un error, intente nuevamente", "error", {
           button: "Aceptar",
         });
       }
+
     }
   }
 })();
