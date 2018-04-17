b0aec118a1ecce432e649b7767bace28652b8004
aaron.acuna <3audiovisual@gmail.com>
2018-04-16 19:53:58 -0600

    Listar paquetes de encargado de sucursal

--- public/components/servicios/servicioUsuarios.service.js
+++ public/components/servicios/servicioUsuarios.service.js
@@ -250,6 +250,7 @@
             
               objPaqueteTemp.setId(objPaquete._id);
 
+              objPaqueteTemp.mostrarEstadoTraslado(objPaquete.estadoTraslado);
               
               listaPaquetes.push(objPaqueteTemp);
       
